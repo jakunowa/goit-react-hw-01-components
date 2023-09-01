8902c6fdf137dbd1bb45790d1c721ba1272daf0f
jakunowa <j.nowacki89@gmail.com>
2023-09-01 19:22:28 +0200

    statistics update

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,9 +1,9 @@
 import PropTypes from 'prop-types';
 import css from './statistics.module.css';
-export const Statistics=({title, stats})=>{
+export const Statistics=({ stats})=>{
     return(
         <section className={css.statistics}>
-  <h2 className={css.title}>{title}</h2>
+  <h2 className={css.title}>Upload stats</h2>
 
   <ul className={css.list}>
   {stats.map(stat => (
@@ -24,7 +24,6 @@ export const Statistics=({title, stats})=>{
 
 
 Statistics.propTypes = {
-  title: PropTypes.string,
   stats: PropTypes.arrayOf(
     PropTypes.shape({
       id: PropTypes.string.isRequired,
